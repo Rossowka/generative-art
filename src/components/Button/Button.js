db2b46277bae271f97ffd55806f619147ecee07e
Sara Rossow <rossowka@icloud.com>
2022-12-20 18:41:17 +0100

    added: tooltip, style to tooltip

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,14 @@ import './styles.scss'
 
 function Button() {
     return(
-        <button className='refresh-button'>
+        <button className='refresh-button tooltip'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
             </svg>
+            <div class="top">
+                <h3>Refresh</h3>
+                <i></i>
+            </div>
         </button>
     )
 }
