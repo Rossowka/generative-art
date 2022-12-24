bffa422f73514fe08f892789b443d019ca7b8b87
Sara Rossow <rossowka@icloud.com>
2022-12-24 11:19:40 +0100

    added: Box component

--- src/components/Art/Box.js
+++ src/components/Art/Box.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './styles.scss';
 
 function Box({ boxes, artSize }) {
     let boxSize = artSize / boxes ;
 
     return (
-        <rect x='0' y='0' width={boxSize} height={boxSize}/>
+        <rect x='0' y='0' width={boxSize} height={boxSize} className='box'/>
     )
 }
 
