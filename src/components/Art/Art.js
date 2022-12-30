b8ac3226dd056b2e4fd97abef2614db6a2d677a4
Sara Rossow <rossowka@icloud.com>
2022-12-30 11:26:49 +0100

    added: rendering boxes

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -5,18 +5,27 @@ import Box from './Box';
 import './styles.scss'
 
 function Art() {
-    const artSize = Math.min(window.innerWidth * 0.9, window.innerHeight * 0.9);
     const random = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    const boxes = (random(3, 12));
+    // to break out from the square pattern you can randomize art size
+    const artSize = Math.min(window.innerWidth * 0.9, window.innerHeight * 0.9);
+    const gridSize = (random(3, 12));
+    const boxSize = artSize / gridSize ;
+
+    let boxes = new Array(gridSize).fill(new Array(gridSize));
+
+    // test
+    console.log('Grid:' + gridSize);
     console.log(boxes);
 
     return (
         <div className='art'>
             <svg width={artSize}>
-                <Box boxes={boxes} artSize={artSize}/>
+                {boxes.map((element, index) => (
+                    <Box key={`box-${index}`} boxSize={boxSize}/>
+                ))}
             </svg>
         </div>
     )
