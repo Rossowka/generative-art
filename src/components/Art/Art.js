f84ba6c769657822b331671257c33ef3f577cd27
Sara Rossow <rossowka@icloud.com>
2023-01-03 13:53:04 +0100

    added: Boxes render in color

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import { colors } from '../../assets';
+import { randomChoiceFrom, randomFromBetween } from '../../utils';
 
 import Box from './Box';
 
-import './styles.scss'
+import './styles.scss';
 
 function Art() {
-    const random = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
     // to break out from the square pattern you can randomize art size
     const artSize = Math.min(window.innerWidth * 0.9, window.innerHeight * 0.9);
-    const gridSize = random(3, 12);
+    const gridSize = randomFromBetween(3, 12);
     const boxSize = artSize / gridSize ;
 
     let boxes = new Array(gridSize).fill(new Array(gridSize).fill(0));
 
+    let colorPalette = colors[randomChoiceFrom(colors)];
+    let backgroundColor = colorPalette[randomChoiceFrom(colorPalette)];
+
     return (
-        <div className='art'>
+        <div className='art' style={{backgroundColor: `#${backgroundColor}90`}}>
             <svg width={artSize}>
                 {boxes.map((array, indexY) => array.map((box, indexX) => (
-                    <Box key={`row-${indexY}-box${indexX}`} indexX={indexX} indexY={indexY} boxSize={boxSize}/>
+                    <Box
+                        key={`row-${indexY}-box${indexX}`}
+                        indexX={indexX}
+                        indexY={indexY}
+                        boxSize={boxSize}
+                        colorPalette={colorPalette}
+                        />
                 )))}
             </svg>
         </div>
--- src/components/Art/Box.js
+++ src/components/Art/Box.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { randomChoiceFrom } from '../../utils';
+
 import './styles.scss';
 
-function Box({ boxSize, indexY, indexX }) {
+function Box({ boxSize, indexY, indexX, colorPalette }) {
+    let backgroundColor = colorPalette[randomChoiceFrom(colorPalette)];
 
     return (
-        <g className='box'>
+        <g className='box' fill={`#${backgroundColor}`}>
             <rect x={boxSize * indexX} y={boxSize * indexY} width={boxSize} height={boxSize}/>
         </g>
     )
