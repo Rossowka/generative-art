63d1fc81af766f075b2f97db887a05a1d6388da9
Sara Rossow <rossowka@icloud.com>
2023-01-03 12:57:10 +0100

    added: initial color palettes

---
+++ src/assets/colors.js
@@ -0,0 +1,14 @@
+let niceColorPalette = [
+    ["8e5572","f2f7f2","c2b97f","bcaa99","88665d"],
+    ["08415c","cc2936","ebbab9","388697","b5ffe1"],
+    ["37123c","71677c","a99f96","dda77b","945d5e"],
+    ["233d4d","fe7f2d","fcca46","a1c181","619b8a"],
+    ["05668d","028090","00a896","02c39a","f0f3bd"],
+    ["664e4c","c1d37f","e2d58b","f0e2a3","f9d4bb"],
+    ["eeb4b3","c179b9","a42cd6","502274","2f242c"],
+    ["502419","7ea172","c7cb85","e7a977","ebbe9b"],
+    ["222222","ffffff","1c5d99","639fab","bbcde5"],
+    ["173753","6daedb","2892d7","1b4353","1d70a2"],
+]
+
+export default niceColorPalette;
