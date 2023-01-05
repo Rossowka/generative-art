768bf2c25a210e802788ed53f9eae39e41e7b2f9
Sara Rossow <rossowka@icloud.com>
2023-01-05 21:15:19 +0100

    added: Triangle component

---
+++ src/components/Art/Triangle.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { getColors } from '../../utils';
+
+function Triangle({ props }) {
+    const { boxSize, indexY, indexX } = props;
+    const [ foregroundColor, backgroundColor ] = getColors(props.colorPalette);
+
+    return (
+        <g>
+            <rect
+                x={boxSize * indexX}
+                y={boxSize * indexY}
+                width={boxSize}
+                height={boxSize}
+                fill={`#${backgroundColor}`}
+                />
+            <path
+                fill={`#${foregroundColor}`}
+                d={`M ${boxSize * indexX} ${boxSize * indexY} v ${boxSize} h ${boxSize} Z`}
+                />
+        </g>
+    )
+}
+
+export default Triangle;
