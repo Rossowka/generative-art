e75a14b28f2a2abdc8e746d78cfffb02a2152696
Sara Rossow <rossowka@icloud.com>
2023-01-06 11:54:00 +0100

    added: Box variations

--- src/components/Art/Box.js
+++ src/components/Art/Box.js
@@ -5,16 +5,33 @@ import './styles.scss';
 
 function Box({ props }) {
     const { boxSize, indexY, indexX } = props;
-    const [ backgroundColor ] = getColors(props.colorPalette);
+    const [ firstColor, secondColor, thirdColor ] = getColors(props.colorPalette);
+
+    const PosX = boxSize * indexX;
+    const PosY = boxSize * indexY;
 
     return (
         <g className='box'>
             <rect
-                x={boxSize * indexX}
-                y={boxSize * indexY}
+                x={PosX}
+                y={PosY}
                 width={boxSize}
                 height={boxSize}
-                fill={`#${backgroundColor}`}
+                fill={`#${firstColor}`}
+                />
+            <rect
+                x={PosX + 0.2 * boxSize}
+                y={PosY + 0.2 * boxSize}
+                width={0.6 * boxSize}
+                height={0.6 * boxSize}
+                fill={`#${secondColor}`}
+                />
+            <rect
+                x={PosX + 0.4 * boxSize}
+                y={PosY + 0.4 * boxSize}
+                width={0.2 * boxSize}
+                height={0.2 * boxSize}
+                fill={`#${thirdColor}`}
                 />
         </g>
     );
